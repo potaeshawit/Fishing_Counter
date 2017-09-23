14741125eb0d0fb633d545deba4046b311176587
Taeshawit Suwanwigo <s.taeshawit@icloud.com>
2017-09-24 03:06:06 +0700

    Transition (Scene / Audio)

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioSource bgMusic;
+	public AudioSource fxMusic;
+	private bool fadingOut = false;
+
+	void Update() {
+		if (fadingOut && bgMusic.volume > 0) {
+			Debug.Log ("music");
+			bgMusic.volume -= (float) (1.1 * Time.deltaTime);
+		}
+	}
+		
+	public void toggleBgMusic() {
+		bgMusic.mute = !bgMusic.mute;
+	}
+
+	public void playFxMusic() {
+		fxMusic.PlayOneShot(fxMusic.clip, 1);
+	}
+
+	public void toggleFxMusic() {
+		fxMusic.mute = !fxMusic.mute;
+	}
+
+	public void FadeOut() {
+		fadingOut = true;
+	}
+}
---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+	public void LoadScene (string sceneName) {
+		StartCoroutine (ChangeLevel (sceneName));
+	}
+
+	IEnumerator ChangeLevel(string sceneName) {
+		float fadeTime = GameObject.Find ("SystemScript").GetComponent<Fader>().BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene (sceneName);
+	}
+}
