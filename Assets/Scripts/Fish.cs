1e81aeeda628331d53dd0f7713af0cbd662ee62e
Taeshawit Suwanwigo <s.taeshawit@icloud.com>
2017-09-26 02:44:50 +0700

    Add all active fish to game

---
+++ Assets/Scripts/Fish.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish {
+
+	static int[] RANGE_X = {-800, 800};
+	static int[] RANGE_Y = {50, -400};
+
+	private int ID;
+	private int timeLimit;
+	private Vector2 initPoint;
+	private int checkPointSize;
+	private List<Vector2> checkPoints;
+	private float speed;
+	private bool facingRight;
+	private bool firstInit;
+
+	public Fish(int ID, int timeLimit, int checkPointSize, float speed = 0f) {
+		this.ID = ID;
+		this.timeLimit = timeLimit;
+		this.checkPointSize = checkPointSize;
+
+		int randForX = Random.Range (0, 2);
+		float randX = (randForX == 0) ? -10.5f : 10.5f;
+		float randY = Random.Range (RANGE_Y[0], RANGE_Y[1]) / 100.0f;
+		initPoint = new Vector2 (randX, randY);
+
+		this.GenCheckPoints ();
+		checkPoints.Add (initPoint);
+
+		facingRight = (initPoint.x < 0);
+		firstInit = true;
+	}
+
+	private void GenCheckPoints() {
+		checkPoints = new List<Vector2> ();
+		for (int i = 0; i < checkPointSize; i++) {
+			float x = Random.Range (RANGE_X[0], RANGE_X[1]) / 100.0f;
+			float y = Random.Range (RANGE_Y[0], RANGE_Y[1]) / 100.0f;
+			Vector2 point = new Vector2 (x, y);
+			checkPoints.Add (point);
+		}
+	}
+
+	public int GetID() {
+		return this.ID;
+	}
+
+	public int GetTimeLimit() {
+		return this.timeLimit;
+	}
+		
+	public Vector2 GetInitPoint() {
+		return this.initPoint;
+	}
+
+	public List<Vector2> GetCheckPoints() { return this.checkPoints; }
+	public void DequeueCheckPoints() {
+		this.checkPoints.RemoveAt (0);
+	}
+
+	public int GetCheckPointSize() { return this.checkPointSize; }
+
+	public float GetSpeed() {
+		return this.speed;
+	}
+
+	public bool GetFacingRight() {
+		return this.facingRight;
+	}
+
+	public bool GetFirstInit() { return this.firstInit; }
+	public void SetFirstInit(bool boolean) { this.firstInit = boolean; }
+}
---
+++ Assets/Scripts/FishManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishManager : MonoBehaviour {
+
+	Animator anim;
+
+	List<Fish> fish;
+	Fish currFish;
+
+	Vector2 speed;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		fish = InitializeFish();
+		currFish = fish [0];
+		speed = new Vector2 ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 1. If first time, move to initPoint
+		if (currFish.GetFirstInit()) {
+			transform.position = currFish.GetInitPoint ();
+			currFish.SetFirstInit (false);
+			return;
+		}
+
+		// 2. Move to each checkpoints
+		MoveFish();
+
+		// 3. reached target
+		if (ReachedTarget()) {
+			RegenNextFish ();
+		}
+	}
+
+	private void MoveFish() {
+		// travel-x
+		Vector2 targetPoint = currFish.GetCheckPoints()[0];
+		float multiplier = GetSpeedMultiplier();
+
+		MoveX (targetPoint.x, multiplier);
+		MoveY (targetPoint.y, multiplier);
+
+		transform.position = GetPositionAtLayer (-8.0f);
+	}
+
+	private void MoveX(float x, float multiplier) {
+		float xDistance = x - transform.position.x;
+		speed.x = Mathf.Abs (xDistance) * multiplier;
+		if (xDistance > 0) {
+			// facing right toward target
+			transform.position += Vector3.right * speed.x * Time.deltaTime;
+			transform.localRotation = Quaternion.Euler(0, 180, 0);
+		} else {
+			transform.position += Vector3.left * speed.x * Time.deltaTime;
+			transform.localRotation = Quaternion.Euler(0, 0, 0);
+		}
+	}
+
+	private void MoveY(float y, float multiplier) {
+		float yDistance = y - transform.position.y;
+		speed.y = Mathf.Abs (yDistance) * multiplier;
+		if (yDistance > 0) {
+			// facing down toward target
+			transform.position += Vector3.up * speed.y * Time.deltaTime;
+		} else {
+			transform.position += Vector3.down * speed.y * Time.deltaTime;
+		}
+	}
+
+	private bool ReachedTarget() {
+		return (speed.x < 0.5 && speed.y < 0.5);
+	}
+
+	private void RegenNextFish() {
+		if (currFish.GetCheckPoints ().Count == 1) {
+			if (fish.Count > 0) {
+				fish.RemoveAt (0);
+				currFish = fish [0];
+				anim.SetInteger ("FishCount", currFish.GetID());
+			}
+		} else {
+			currFish.DequeueCheckPoints ();
+		}
+	}
+
+	private float GetSpeedMultiplier() {
+		return (currFish.GetCheckPoints ().Count == currFish.GetCheckPointSize ()) ? 0.2f : 0.7f;
+	}
+
+	private Vector3 GetPositionAtLayer(float z) {
+		return (new Vector3 (transform.position.x, transform.position.y, z));
+	}
+		
+	private List<Fish> InitializeFish() {
+		List<Fish> f = new List<Fish> ();
+		for (int i = 1;  i <= 4;  i++) f.Add (new Fish (i, 8, 6));
+		for (int i = 5;  i <= 8;  i++) f.Add (new Fish (i, 8, 6));
+		for (int i = 9;  i <= 12; i++) f.Add (new Fish (i, 7, 6));
+		for (int i = 13; i <= 16; i++) f.Add (new Fish (i, 6, 6));
+		for (int i = 17; i <= 20; i++) f.Add (new Fish (i, 6, 6));
+		return f;
+	}
+}
